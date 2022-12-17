61341363047591d334059f0cde83e1d079d26070
Md Irfan <iamhero337@gmail.com>
2022-12-17 18:19:54 +0530

    updated

---
+++ inputhello.py
@@ -0,0 +1,10 @@
+#take the input of the from user it also prints the string which you gave with it 
+name = input("What is your name?: ")
+
+#that's how you print the input you just took in the same line of function which you called 
+print("Hello,", name)
+print(name)
+
+'So just learnt that this is a comment'
+"""And this too"""
+"what about this"
